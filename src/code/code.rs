cc9b784777bfde5e9f3b1fa8c82340c55c035419
YSawc <niceage6690@gmail.com>
2020-07-10 06:16:53 +0900

    Add opecodes.

--- src/code/code.rs
+++ src/code/code.rs
@@ -8,23 +8,23 @@ pub fn read_ope_code(v: &Vec<u8>) -> Option<u8> {
 
     for mut n in 0..v.len() {
         match v[n] {
-            2..=8 => {
+            Inst::iconst_m1..=Inst::iconst_5 => {
                 stack_machine.imm.push(v[n] - 3);
             }
-            16 => {
+            Inst::bipush => {
                 n += 1;
                 stack_machine.imm.push(v[n]);
             }
-            27 => stack_machine.imm.push(stack_machine.i_st1 as u8),
-            28 => stack_machine.imm.push(stack_machine.i_st2 as u8),
-            60 => stack_machine.i_st1 = stack_machine.imm.pop()? as i8,
-            61 => stack_machine.i_st2 = stack_machine.imm.pop()? as i8,
-            62 => stack_machine.i_st3 = stack_machine.imm.pop()? as i8,
-            96 => {
+            Inst::iload_1 => stack_machine.imm.push(stack_machine.i_st1 as u8),
+            Inst::iload_2 => stack_machine.imm.push(stack_machine.i_st2 as u8),
+            Inst::istore_1 => stack_machine.i_st1 = stack_machine.imm.pop()? as i8,
+            Inst::istore_2 => stack_machine.i_st2 = stack_machine.imm.pop()? as i8,
+            Inst::istore_3 => stack_machine.i_st3 = stack_machine.imm.pop()? as i8,
+            Inst::iadd => {
                 let tmp = stack_machine.imm.pop()? + stack_machine.imm.pop()?;
                 stack_machine.imm.push(tmp);
             }
-            177 => {
+            Inst::_return => {
                 println!("stack_matchine : {:?}", stack_machine);
                 return None;
             }
@@ -33,4 +33,29 @@ pub fn read_ope_code(v: &Vec<u8>) -> Option<u8> {
     }
 
     Some(0)
+}
+
+#[allow(non_upper_case_globals)]
+#[allow(non_snake_case)]
+#[allow(dead_code)]
+mod Inst {
+    pub type Code = u8;
+    pub const iconst_m1: u8 = 2;
+    pub const iconst_0: u8 = 3;
+    pub const iconst_1: u8 = 4;
+    pub const iconst_2: u8 = 5;
+    pub const iconst_3: u8 = 6;
+    pub const iconst_4: u8 = 7;
+    pub const iconst_5: u8 = 8;
+    pub const bipush: u8 = 16;
+    pub const iload_0: u8 = 26;
+    pub const iload_1: u8 = 27;
+    pub const iload_2: u8 = 28;
+    pub const iload_3: u8 = 29;
+    pub const istore_0: u8 = 59;
+    pub const istore_1: u8 = 60;
+    pub const istore_2: u8 = 61;
+    pub const istore_3: u8 = 62;
+    pub const iadd: u8 = 96;
+    pub const _return: u8 = 177;
 }
